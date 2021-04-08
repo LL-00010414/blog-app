6e910bc64b678640465a236635ddc4a27f99e516
00010414 <00010414.ll@gmail.com>
2021-04-08 11:01:12 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Blog application
+
+### This web application allows users to create,edit and delete posts
+
+To run this application you must follow the instructions below:
+
+1. Clone this repository
+```bash
+git clone [repository link]
+```
+
+2. Install dependencies
+```bash
+cd [project folder]
+npm install
+```
+
+3. Run the application
+```bash
+node app
+```
+
+### Web application dependencies
+ - express.js
+ - pug.js 
+
+### Web application repository on github
+[link to github repo]
---
+++ app.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const app = express()
+
+const fs = require('fs')
+
+app.set('view engine', 'pug')
+
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({extended: false}))
+
+// localhost:8000 
+app.get('/', (req, res) => {
+    res.render('home')
+})
+ 
+app.get('/create', (req, res) => {
+    res.render('create')
+})
+
+app.post('/create', (req, res) => {
+    const title = req.body.title
+    const description = req.body.description
+
+    if (title.trim() === '' && description.trim() ==='') {
+        res.render('create', { error: true})
+    }
+    else {
+        fs.readFile('./data/posts.json', (err, data) => {
+            if (err) throw err
+
+            const posts = JSON.parse(data)
+
+            posts.push({
+                id: id (),
+                title: title,
+                description: description,
+            })
+
+            fs.writeFile('./data/posts.json', JSON.stringify(posts), err => {
+                if (err) throw err
+
+                res.render('create', {success: true})
+            })
+        })
+    }
+})
+
+/*basic REST API*/
+
+app.get('/api/v1/posts', (req, res) => {
+    fs.readFile('./data/posts.json', (err, data) => {
+        if (err) throw err
+
+        const posts = JSON.parse(data)
+
+        res.json(posts)
+    })
+
+})
+
+
+
+app.get('/posts', (req, res) => {
+
+    fs.readFile('./data/posts.json', (err, data) => {
+        if (err) throw err
+
+        const posts = JSON.parse(data)
+
+        res.render('posts', { posts:posts })
+    })
+})
+
+app.get("/posts/:id", (req, res) => {
+    const id = req.params.id
+
+    fs.readFile('./data/posts.json', (err, data) => {
+        if (err) throw err
+
+
+        const posts = JSON.parse(data)
+
+        const post = posts.filter(post => post.id ==id) [0]
+
+        res.render("detail", {post: post})
+
+  
+    })
+})
+
+app.listen(8000, err => {
+    if (err) console.log(err)
+
+    console.log('Server is running on port 8000...')
+})
+
+
+function id () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  }
